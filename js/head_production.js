13f04e5d1b7794e516fb05d197e6e2a19cdefc28
wmlhard <1337232751@qq.com>
2017-08-20 00:53:05 +0800

    version

---
+++ js/head_production.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/8/20 0020.
+ */
+document.onscroll=function(){
+    var tit = document.getElementsByTagName("nav")[0];
+    var tittop=tit.offsetTop;
+    var navimg=tit.getElementsByTagName("img")[0];
+    var btop = document.body.scrollTop||document.documentElement.scrollTop;
+    if(btop>tittop){
+        tit.setAttribute("style","background-color:white;color:black;border:1px solid black;");
+        navimg.setAttribute("src","../img/iphone_logo02.png");
+    }
+    else{
+        tit.setAttribute("style","");
+        navimg.setAttribute("src","../img/protect_images/logo.png");
+    }
+
+}
