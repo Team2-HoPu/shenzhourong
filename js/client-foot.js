bf03cb83b03199ba77edc9fc3a59888f32f46ff6
e <www.1132618033@qq.com>
2017-08-20 00:26:40 +0800

    versoe0.0.5

--- js/client-foot.js
+++ js/client-foot.js
@@ -1,3 +1,23 @@
 /**
  * Created by Administrator on 2017/8/17.
  */
+
+window.onload=function(){
+    var foot_img=document.getElementById("foot-img");
+    foot_img.addEventListener("DOMMouseScroll",function(){
+        scrollFunc();
+    },false);
+
+   function scrollFunc(){
+       var bg_img=parseInt(foot_img.style.backgroundSize);
+       console.log(bg_img);
+       var e=event||window.event;
+       if(e.wheelDelta){//IE/Opera/Chrome
+           bg_img=parseInt(foot_img.style.width)+20;
+       }else if(e.detail){//Firefox
+           bg_img=parseInt(foot_img.style.width)+20;
+       }
+       foot_img.style.backgroundSize=bg_img+"%";
+       console.log(foot_img.style.backgroundSize);
+   }
+}
--- js/fn-tools.js
+++ js/fn-tools.js
@@ -1,3 +1,17 @@
 /**
  * Created by Administrator on 2017/8/18.
  */
+window.onload=function(){
+    var obtn=document.getElementById("sidabar-center-return");
+    var clientHeight=document.documentElement.clientHeight;//可视区域的高度
+    window.onscroll=function(){//onscroll为滚动滚动条事件，即这个函数在滚动条滚动时触发
+
+        var osTop=document.documentElement.scrollTop||document.body.scrollTop;//scrollTop为滚动条滑动的距离
+        if (osTop>=clientHeight) {
+            obtn.style.display='block'
+        }
+        else{
+            obtn.style.display='none'
+        }
+    };
+};
