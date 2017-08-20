5fc4ba6d856b8d475d25bb94a9b42e3850f68fc6
wmlhard <1337232751@qq.com>
2017-08-20 21:43:58 +0800

    version

--- js/production.js
+++ js/production.js
@@ -5,12 +5,20 @@ window.onload=function(){
     var prev=getObj("prev");
     console.log(next,prev);
     var production_third_carousel=getObj("production_third_carousel");
+    var buttons=document.getElementsByTagName("span");
     prev.addEventListener('click',function(){
         moving(1200,0,"-2400px");
     });
     next.addEventListener('click',function(){
         moving(-1200,-2400,"0");
     });
+    for(var i=0;i<buttons.length;i++){
+        (function(n){
+            buttons[n].onclick=function(){
+                button_control(this,n);
+            }
+        })(i)
+    }
     function moving (speed,terminal,flag){
         var left_value=parseInt(production_third_carousel.style.left);
         console.log(left_value);
@@ -20,7 +28,23 @@ window.onload=function(){
                 left_value+=speed;
                 production_third_carousel.style.left=left_value+"px";
             }
+        for(var i=0;i<buttons.length;i++){
+            buttons[i].setAttribute("class","");
+        }
+        buttons[left_value/-1200].setAttribute("class","on");
     }
+    function button_control(that,num) {
+        //在修改class前，清空所有class；
+        for(var i=0;i<buttons.length;i++){
+            buttons[i].setAttribute("class","");
+        }
+        //点击span修改class为“on”；
+        that.setAttribute("class","on");
+        //控制图片移动到当前位置
+//            console.log(num);
+        list.style.left=num*(-1200)+"px";
+    }
+
     timer=setInterval(function(){
         moving(-1200,-2400,"0");
     },2000);
