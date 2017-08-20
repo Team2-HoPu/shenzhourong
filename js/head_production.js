83f4635b7a07d4dc712a68309e5ed972672de7f3
wmlhard <1337232751@qq.com>
2017-08-20 11:05:54 +0800

    version

--- js/head_production.js
+++ js/head_production.js
@@ -1,6 +1,7 @@
 /**
  * Created by Administrator on 2017/8/20 0020.
  */
+//控制顶部页面的js部分
 document.onscroll=function(){
     var tit = document.getElementsByTagName("nav")[0];
     var tittop=tit.offsetTop;
@@ -8,7 +9,7 @@ document.onscroll=function(){
     var btop = document.body.scrollTop||document.documentElement.scrollTop;
     if(btop>tittop){
         tit.setAttribute("style","background-color:white;color:black;border:1px solid black;");
-        navimg.setAttribute("src","../img/iphone_logo02.png");
+        navimg.setAttribute("src","../img/protect_images/iphone_logo02.png");
     }
     else{
         tit.setAttribute("style","");
