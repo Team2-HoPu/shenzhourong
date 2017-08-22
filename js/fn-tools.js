f97a4c9953e6f254cef5169cc16dc8168065ca11
slilight <1584513926@qq.com>
2017-08-22 09:58:52 +0800

    versin0.0.8

--- js/fn-tools.js
+++ js/fn-tools.js
@@ -11,7 +11,9 @@
         obtn.style.display='block';
         obtn.addEventListener("click",function(){
             obtn.style.display='block';
-            document.body.scrollTop-=100;
+            var turn_scrolltop=document.body.scrollTop-=500;
+            var scrolltop_e=document.documentElement.scrollTop-=800
+            turn_scrolltop||scrolltop_e;
         });
     }
     else if(osTop<=100){
@@ -27,23 +29,23 @@
 //-----------************************-----------------------
 //底部动画
 function foot_anima () {
-    var scrollTop=document.body.scrollTop;
-    var foot_imgpic=window.frames["foot_animation"].document.getElementsByClassName("foot_imgPic")[0];
-    console.log("foot_imgpic:"+foot_imgpic);
-    console.log(scrollTop);
-
-    if(scrollTop>=1500){
-        foot_imgpic.style.transition="all 1.5s";
-        foot_imgpic.style.transform="translate(0,20px)";
-    }
-    else if(scrollTop<1500){
-        foot_imgpic.style.transition="all 1.5s";
-        foot_imgpic.style.transform="translate(0,-20px)";
-    }
-    else
-    {   foot_imgpic.style.transition="";
-        foot_imgpic.style.transform="";
-    }
+var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+// var foot_imgpic=document.getElementById('foot_animation').contentWindow.document.getElementsByClassName('foot_imgPic')[0];
+var foot_imgpic=document.getElementsByClassName("foot_imgPic")[0];
+console.log("foot_imgpic:"+foot_imgpic);
+console.log(scrollTop);
+if(scrollTop>=1500){
+    foot_imgpic.style.transition="all 1.5s";
+    foot_imgpic.style.transform="translate(0,20px)";
+}
+else if(scrollTop<1500){
+    foot_imgpic.style.transition="all 1.5s";
+    foot_imgpic.style.transform="translate(0,-20px)";
+}
+else
+{   foot_imgpic.style.transition="";
+    foot_imgpic.style.transform="";
+}
 }
 //底部动画结束
 //----------------*****************************---------------------------
